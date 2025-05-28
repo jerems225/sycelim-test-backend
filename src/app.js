59e217b84a722e78e8fea430bee3e87a763a5951
Jeremy Niondo <jeremieniondo@gmail.com>
2025-05-29 00:14:50 +0100

    Upload directory

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/properties/cover', express.static(path.join(__dirname, 'cover')));
+app.use('/public/cover', express.static(path.join(__dirname, 'cover')));
 
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
